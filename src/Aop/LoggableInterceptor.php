d10ca7b43b6c17d96610dae865da227ba2d0abfb
David Akopyan <david@akopyan.lv>
2022-10-03 14:10:55 +0300

    - sf serializer for logging objects

--- src/Aop/LoggableInterceptor.php
+++ src/Aop/LoggableInterceptor.php
@@ -7,12 +7,15 @@ namespace DMP\LoggableBundle\Aop;
 use CG\Proxy\MethodInterceptorInterface;
 use CG\Proxy\MethodInvocation;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 use Throwable;
 
 class LoggableInterceptor implements MethodInterceptorInterface
 {
 
-    public function __construct(private readonly LoggerInterface $logger)
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly SerializerInterface $serializer)
     {}
 
     /**
@@ -22,22 +25,21 @@ class LoggableInterceptor implements MethodInterceptorInterface
      */
     public function intercept(MethodInvocation $invocation): mixed
     {
-        $this->logger->debug("'$invocation' was invoked", [
-            'arguments' => $invocation->arguments,
+
+        $this->logger->debug(sprintf('%s was invoked', $invocation), [
+            'arguments' => $this->serializer->serialize($invocation->arguments, 'json'),
         ]);
         try {
             $return = $invocation->proceed();
 
-            $this->logger->debug("'$invocation' has finished executing", [
-                'returnValue' => $return,
+            $this->logger->debug(sprintf('%s has finished executing', $invocation), [
+                'returnValue' => $this->serializer->serialize($return, 'json'),
             ]);
-
             return $return;
         } catch (Throwable $throwable) {
-            $this->logger->warning("'$invocation' has failed to execute", [
+            $this->logger->debug(sprintf('%s has failed to execute', $invocation), [
                 'throwable' => $throwable,
             ]);
-
             throw $throwable;
         }
     }
